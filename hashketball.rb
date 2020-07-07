e2afcd43d0eed9df29e8cb0371479e0d37bfa116
Penn M Diba <pdiba@oxbowschool.org>
2020-07-07 05:52:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,19 @@
 # Write your code here!
 def game_hash
-{}
+hash={{home:
+  {team_name:'', 
+  colors:['',''], 
+  players:[
+    {player_name:,
+    number:
+    shoe:
+    points:
+    rebounds:
+    assists:
+    steals:
+    blocks:
+    slam_dunks:
+    }]
+{away:
+}
 end
