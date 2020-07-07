bbcfa9e568ce66c7f9b18427e27987cfa326bc90
Penn M Diba <pdiba@oxbowschool.org>
2020-07-07 06:21:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,18 @@
 # Write your code here!
 def game_hash
-{
-  {home:
-    {team_name:'Brooklyn Nets', 
-    colors:['Black','White'], 
-    players:[
-      {player_name:'Alan Anderson',
-      number:0,
-      shoe:16,
-      points:22,
-      rebounds:12,
+  {
+    {
+      home:
+      {
+        team_name:'Brooklyn Nets', 
+        colors:['Black','White'], 
+        players:[
+          {
+            player_name:'Alan Anderson',
+            number:0,
+            shoe:16,
+            points:22,
+            rebounds:12,
       assists:12,
       steals:3,
       blocks:1,
