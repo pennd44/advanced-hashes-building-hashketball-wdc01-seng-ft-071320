3154493b4fb035b84838c1059086e99863b0c528
Penn M Diba <pdiba@oxbowschool.org>
2020-07-07 06:17:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,10 +54,9 @@ def game_hash
       steals:4,
       blocks:11,
       slam_dunks:1 
-      },
-       ]
+      }
+      ]
     }
-  }
   away:
   {team_name:'Charlotte Hornets', 
     colors:['Turquoise','Purple'], 
@@ -111,8 +110,8 @@ def game_hash
       steals:7,
       blocks:5,
       slam_dunks:12
-      },
-       ]
+      }
+      ]
+    }
   }
-}
 end
